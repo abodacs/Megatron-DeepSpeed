8a2a7de509377bdfa88f98415b8180bf9e3bcabc
Abdullah Mohammed <554032+abodacs@users.noreply.github.com>
2022-01-21 15:24:28 +0000

    enable new_style for add_scalar function for faster data format

--- megatron/global_vars.py
+++ megatron/global_vars.py
@@ -18,7 +18,7 @@
 import os
 import sys
 import time
-
+import functools
 from pathlib import Path
 
 import torch
@@ -35,9 +35,10 @@ _GLOBAL_CODECARBON_TRACKER = None
 _GLOBAL_ADLR_AUTORESUME = None
 _GLOBAL_TIMERS = None
 
+
 def get_args():
     """Return arguments."""
-    _ensure_var_is_initialized(_GLOBAL_ARGS, 'args')
+    _ensure_var_is_initialized(_GLOBAL_ARGS, "args")
     return _GLOBAL_ARGS
 
 
@@ -50,13 +51,12 @@ def get_current_global_batch_size():
 
 
 def update_num_microbatches(consumed_samples, consistency_check=True):
-    _GLOBAL_NUM_MICROBATCHES_CALCULATOR.update(consumed_samples,
-                                               consistency_check)
+    _GLOBAL_NUM_MICROBATCHES_CALCULATOR.update(consumed_samples, consistency_check)
 
 
 def get_tokenizer():
     """Return tokenizer."""
-    _ensure_var_is_initialized(_GLOBAL_TOKENIZER, 'tokenizer')
+    _ensure_var_is_initialized(_GLOBAL_TOKENIZER, "tokenizer")
     return _GLOBAL_TOKENIZER
 
 
@@ -65,10 +65,12 @@ def get_tensorboard_writer():
     to check if it is initialized."""
     return _GLOBAL_TENSORBOARD_WRITER
 
+
 def get_codecarbon_tracker():
     """Return codecarbon tracker. It can be None so no need
     to check if it is initialized."""
     return _GLOBAL_CODECARBON_TRACKER
+
 
 def get_adlr_autoresume():
     """ADLR autoresume object. It can be None so no need
@@ -78,16 +80,15 @@ def get_adlr_autoresume():
 
 def get_timers():
     """Return timers."""
-    _ensure_var_is_initialized(_GLOBAL_TIMERS, 'timers')
+    _ensure_var_is_initialized(_GLOBAL_TIMERS, "timers")
     return _GLOBAL_TIMERS
 
 
-def set_global_variables(extra_args_provider=None, args_defaults={},
-                         ignore_unknown_args=False):
+def set_global_variables(extra_args_provider=None, args_defaults={}, ignore_unknown_args=False):
     """Set args, tokenizer, tensorboard-writer, adlr-autoresume, and timers."""
-    args = _parse_args(extra_args_provider=extra_args_provider,
-                       defaults=args_defaults,
-                       ignore_unknown_args=ignore_unknown_args)
+    args = _parse_args(
+        extra_args_provider=extra_args_provider, defaults=args_defaults, ignore_unknown_args=ignore_unknown_args
+    )
     _build_num_microbatches_calculator(args)
     if args.vocab_file or args.tokenizer_name_or_path:
         _ = _build_tokenizer(args)
@@ -97,31 +98,28 @@ def set_global_variables(extra_args_provider=None, args_defaults={},
     _set_timers()
 
 
-def _parse_args(extra_args_provider=None, defaults={},
-                ignore_unknown_args=False):
+def _parse_args(extra_args_provider=None, defaults={}, ignore_unknown_args=False):
     """Parse entire arguments."""
     global _GLOBAL_ARGS
-    _ensure_var_is_not_initialized(_GLOBAL_ARGS, 'args')
-    _GLOBAL_ARGS = parse_args(extra_args_provider=extra_args_provider,
-                              defaults=defaults,
-                              ignore_unknown_args=ignore_unknown_args)
+    _ensure_var_is_not_initialized(_GLOBAL_ARGS, "args")
+    _GLOBAL_ARGS = parse_args(
+        extra_args_provider=extra_args_provider, defaults=defaults, ignore_unknown_args=ignore_unknown_args
+    )
     return _GLOBAL_ARGS
 
 
 def _build_num_microbatches_calculator(args):
 
     global _GLOBAL_NUM_MICROBATCHES_CALCULATOR
-    _ensure_var_is_not_initialized(_GLOBAL_NUM_MICROBATCHES_CALCULATOR,
-                                   'num microbatches calculator')
+    _ensure_var_is_not_initialized(_GLOBAL_NUM_MICROBATCHES_CALCULATOR, "num microbatches calculator")
 
-    _GLOBAL_NUM_MICROBATCHES_CALCULATOR = build_num_microbatches_calculator(
-        args)
+    _GLOBAL_NUM_MICROBATCHES_CALCULATOR = build_num_microbatches_calculator(args)
 
 
 def _build_tokenizer(args):
     """Initialize tokenizer."""
     global _GLOBAL_TOKENIZER
-    _ensure_var_is_not_initialized(_GLOBAL_TOKENIZER, 'tokenizer')
+    _ensure_var_is_not_initialized(_GLOBAL_TOKENIZER, "tokenizer")
     _GLOBAL_TOKENIZER = build_tokenizer(args)
     return _GLOBAL_TOKENIZER
 
@@ -135,35 +133,41 @@ def rebuild_tokenizer(args):
 def _set_tensorboard_writer(args):
     """Set tensorboard writer."""
     global _GLOBAL_TENSORBOARD_WRITER
-    _ensure_var_is_not_initialized(_GLOBAL_TENSORBOARD_WRITER,
-                                   'tensorboard writer')
+    _ensure_var_is_not_initialized(_GLOBAL_TENSORBOARD_WRITER, "tensorboard writer")
 
-    if hasattr(args, 'tensorboard_dir') and \
-       args.tensorboard_dir and args.rank == (args.world_size - 1):
+    if hasattr(args, "tensorboard_dir") and args.tensorboard_dir and args.rank == (args.world_size - 1):
         try:
             from torch.utils.tensorboard import SummaryWriter
-            print('> setting tensorboard ...')
+
+            print("> setting tensorboard ...")
             _GLOBAL_TENSORBOARD_WRITER = SummaryWriter(
-                log_dir=args.tensorboard_dir,
-                max_queue=args.tensorboard_queue_size)
+                log_dir=args.tensorboard_dir, max_queue=args.tensorboard_queue_size
+            )
+            _GLOBAL_TENSORBOARD_WRITER.add_scalar = functools.partial(
+                _GLOBAL_TENSORBOARD_WRITER.add_scalar, new_style=True
+            )
         except ModuleNotFoundError:
-            print('WARNING: TensorBoard writing requested but is not '
-                  'available (are you using PyTorch 1.1.0 or later?), '
-                  'no TensorBoard logs will be written.', flush=True)
+            print(
+                "WARNING: TensorBoard writing requested but is not "
+                "available (are you using PyTorch 1.1.0 or later?), "
+                "no TensorBoard logs will be written.",
+                flush=True,
+            )
 
 
 # Important: the codecarbon is very unstable and its latest incarnation using the python scheduler interferes with the asyncio library we use in the test suite which breaks everything, so making this a no-op for now.
 def _set_codecarbon_tracker(args):
 
-    return # turned off
+    return  # turned off
 
     global _GLOBAL_CODECARBON_TRACKER
-    if not hasattr(args, 'codecarbon_dir') or args.codecarbon_dir is None:
+    if not hasattr(args, "codecarbon_dir") or args.codecarbon_dir is None:
         return
 
     import codecarbon
+
     if args.rank == 0:
-        print('> setting codecarbon ...')
+        print("> setting codecarbon ...")
 
     output_dir = args.codecarbon_dir
     output_file = f"emissions-{args.rank:03d}.csv"
@@ -192,53 +196,53 @@ def _set_codecarbon_tracker(args):
 
 def codecarbon_tracker_start():
 
-    return # turned off, see the notes above
+    return  # turned off, see the notes above
 
     global _GLOBAL_CODECARBON_TRACKER
     if _GLOBAL_CODECARBON_TRACKER is None:
         return
 
-    #print("CC START")
+    # print("CC START")
     _GLOBAL_CODECARBON_TRACKER.start()
 
 
 def codecarbon_tracker_stop():
 
-    return # turned off, see the notes above
+    return  # turned off, see the notes above
 
     global _GLOBAL_CODECARBON_TRACKER
     if _GLOBAL_CODECARBON_TRACKER is None:
         return
 
-    #print("CC STOP")
+    # print("CC STOP")
     _GLOBAL_CODECARBON_TRACKER.stop()
 
 
 def codecarbon_tracker_flush():
 
-    return # turned off, see the notes above
+    return  # turned off, see the notes above
 
     global _GLOBAL_CODECARBON_TRACKER
     if _GLOBAL_CODECARBON_TRACKER is None:
         return
 
-    #print("CC FLUSH")
+    # print("CC FLUSH")
     _GLOBAL_CODECARBON_TRACKER.flush()
 
 
 def _set_adlr_autoresume(args):
     """Initialize ADLR autoresume."""
     global _GLOBAL_ADLR_AUTORESUME
-    _ensure_var_is_not_initialized(_GLOBAL_ADLR_AUTORESUME, 'adlr autoresume')
+    _ensure_var_is_not_initialized(_GLOBAL_ADLR_AUTORESUME, "adlr autoresume")
 
     if args.adlr_autoresume:
         if args.rank == 0:
-            print('enabling autoresume ...', flush=True)
-        sys.path.append(os.environ.get('SUBMIT_SCRIPTS', '.'))
+            print("enabling autoresume ...", flush=True)
+        sys.path.append(os.environ.get("SUBMIT_SCRIPTS", "."))
         try:
             from userlib.auto_resume import AutoResume
         except BaseException:
-            print('ADLR autoresume is not available, exiting ...')
+            print("ADLR autoresume is not available, exiting ...")
             sys.exit()
 
         _GLOBAL_ADLR_AUTORESUME = AutoResume
@@ -247,18 +251,18 @@ def _set_adlr_autoresume(args):
 def _set_timers():
     """Initialize timers."""
     global _GLOBAL_TIMERS
-    _ensure_var_is_not_initialized(_GLOBAL_TIMERS, 'timers')
+    _ensure_var_is_not_initialized(_GLOBAL_TIMERS, "timers")
     _GLOBAL_TIMERS = Timers()
 
 
 def _ensure_var_is_initialized(var, name):
     """Make sure the input variable is not None."""
-    assert var is not None, '{} is not initialized.'.format(name)
+    assert var is not None, "{} is not initialized.".format(name)
 
 
 def _ensure_var_is_not_initialized(var, name):
     """Make sure the input variable is not None."""
-    assert var is None, '{} is already initialized.'.format(name)
+    assert var is None, "{} is already initialized.".format(name)
 
 
 class _Timer:
@@ -272,16 +276,16 @@ class _Timer:
 
     def start(self):
         """Start the timer."""
-        assert not self.started_, 'timer has already been started'
+        assert not self.started_, "timer has already been started"
         torch.cuda.synchronize()
         self.start_time = time.time()
         self.started_ = True
 
     def stop(self):
         """Stop the timer."""
-        assert self.started_, 'timer is not started'
+        assert self.started_, "timer is not started"
         torch.cuda.synchronize()
-        self.elapsed_ += (time.time() - self.start_time)
+        self.elapsed_ += time.time() - self.start_time
         self.started_ = False
 
     def reset(self):
@@ -325,22 +329,20 @@ class Timers:
         assert normalizer > 0.0
         for name in names:
             value = self.timers[name].elapsed(reset=reset) / normalizer
-            writer.add_scalar(f'time/{name}-time', value, iteration)
+            writer.add_scalar(f"time/{name}-time", value, iteration)
 
     def log(self, names, normalizer=1.0, reset=True):
         """Log a group of timers."""
         assert normalizer > 0.0
-        string = ''
+        string = ""
         for name in names:
-            elapsed_time = self.timers[name].elapsed(
-                reset=reset) * 1000.0 / normalizer
-            string += ' | {}: {:.2f}'.format(name, elapsed_time)
+            elapsed_time = self.timers[name].elapsed(reset=reset) * 1000.0 / normalizer
+            string += " | {}: {:.2f}".format(name, elapsed_time)
         if not len(string):
             return
-        string = 'time (ms)' + string
+        string = "time (ms)" + string
         if torch.distributed.is_initialized():
-            if torch.distributed.get_rank() == (
-                    torch.distributed.get_world_size() - 1):
+            if torch.distributed.get_rank() == (torch.distributed.get_world_size() - 1):
                 print(string, flush=True)
         else:
             print(string, flush=True)
